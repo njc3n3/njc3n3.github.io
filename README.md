f5f5ea26b2ccec29a65a6215a7cd8f61d513fb08
Nick <nicholasjcoffey@gmail.com>
2020-12-16 17:39:49 -0600

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-
-A simple site meant to act as my personal resume/portfolio.
-
-[Link](https://nickcoffey.github.io/)
--- src/components/about/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as AboutContent } from './AboutContent'
-export { default as AboutList } from './AboutList'
-export { default as AboutMe } from './AboutMe'
-export { default as ContactMe } from './ContactMe'
-export { default as Education } from './Education'
-export { default as PastProjects } from './PastProjects'
-export { TechnicalSkills } from './TechnicalSkills'
-export { default as WorkExperience } from './WorkExperience'
