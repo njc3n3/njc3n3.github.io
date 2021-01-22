2a01826943b67b31144b5e9efeefff976996f181
Nick Coffey <nicholasjcoffey@gmail.com>
2021-01-22 11:06:50 -0600

    can delete drafts

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -3,7 +3,7 @@ import moment from 'moment'
 import styled, { css } from 'styled-components'
 import { Link, useHistory } from 'react-router-dom'
 import { useMutation, useQuery } from '../hooks'
-import { Button, Content, Input } from '../components/general'
+import { Button, Content, Input, Modal } from '../components/general'
 import { ThemeContext } from '..'
 import { largeScreenMixin, transitionMixin } from '../styles'
 import { AuthContext } from '../App'
@@ -70,6 +70,10 @@ const StyledLink = styled(Link)<{ hoverColor: string }>`
   }
 `
 
+const DeleteContainer = styled.div<{ spacing: number }>`
+  padding: ${({ spacing }) => spacing}rem;
+`
+
 export default function Posts() {
   const history = useHistory()
   const { isLoggedIn, getToken } = useContext(AuthContext)
@@ -80,8 +84,13 @@ export default function Posts() {
   const [searchText, setSearchText] = useState('')
   const [searchTag, setSearchTag] = useState('')
 
+  const [deletePost, setDeletePost] = useState<{ id: string; title: string }>()
+  const { runMutation: deleteDraft } = useMutation(`posts?id=${deletePost?.id}`, 'delete', () => {
+    window.location.reload() //refresh page
+  })
+
   const { data, loading, error } = useQuery<{ posts: PostType[] }>(url, undefined, getToken() || undefined)
-  const { runMutation } = useMutation('posts', 'put', res => {
+  const { runMutation: makeDraft } = useMutation('posts', 'put', res => {
     if (res.data) {
       console.log('SUCCESS')
       setShowDrafts(true)
@@ -120,7 +129,7 @@ export default function Posts() {
     if (showDrafts) {
       history.push(`edit-draft/${post.id}`)
     } else {
-      runMutation({ ...post, isPublished: false }, getToken() || undefined)
+      makeDraft({ ...post, isPublished: false }, getToken() || undefined)
     }
   }
 
@@ -138,60 +147,90 @@ export default function Posts() {
     const calcSpacing = `${mainSpacing / 2}rem`
 
     content = (
-      <Container spacing={mainSpacing}>
-        <Content style={{ marginBottom: mainSpacingRem }}>
-          <h3 style={{ marginBottom: mainSpacingRem }}>Search for {showDrafts ? 'drafts' : 'posts'}</h3>
-          <StyledForm spacing={mainSpacingRem}>
-            <div className='input-group'>
-              <strong>Text: </strong>
-              <Input
-                placeholder='Enter search text here...'
-                value={searchText}
-                onChange={e => setSearchText(e.target.value)}
-                className='text-input'
-              />
-              <strong>Tags: </strong>
-              <select value={searchTag} onChange={e => setSearchTag(e.target.value)}>
-                {availableTags?.map((tag, index) => (
-                  <option key={index} value={tag}>
-                    {tag}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className='btn-group'>
-              <Button onClick={e => submitForm()}>Search</Button>
-              <Button onClick={e => clearForm()} color='secondary'>
-                Clear
-              </Button>
-              {isLoggedIn() && (
-                <Button onClick={() => setShowDrafts(!showDrafts)}>Show {showDrafts ? 'posts' : 'drafts'}</Button>
-              )}
-            </div>
-          </StyledForm>
-        </Content>
-        {data?.posts.map(({ title, tags, created, id }, index) => (
-          <Content key={index}>
-            <StyledLink hoverColor={darkSubtitleText} to={`/posts/${id}`}>
-              <h1 style={{ marginBottom: calcSpacing }}>{title}</h1>
-            </StyledLink>
-            <p style={{ marginBottom: calcSpacing }}>{moment(created).format('MMMM Do, YYYY')}</p>
-            {tags && (
-              <p style={{ color: darkSubtitleText }}>
-                {tags.reduce(
-                  (tagString, tag, index) => (tagString += `#${tag}${index !== tags.length - 1 ? ', ' : ''}`),
-                  ''
+      <>
+        <Container spacing={mainSpacing}>
+          <Content style={{ marginBottom: mainSpacingRem }}>
+            <h3 style={{ marginBottom: mainSpacingRem }}>Search for {showDrafts ? 'drafts' : 'posts'}</h3>
+            <StyledForm spacing={mainSpacingRem}>
+              <div className='input-group'>
+                <strong>Text: </strong>
+                <Input
+                  placeholder='Enter search text here...'
+                  value={searchText}
+                  onChange={e => setSearchText(e.target.value)}
+                  className='text-input'
+                />
+                <strong>Tags: </strong>
+                <select value={searchTag} onChange={e => setSearchTag(e.target.value)}>
+                  {availableTags?.map((tag, index) => (
+                    <option key={index} value={tag}>
+                      {tag}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className='btn-group'>
+                <Button onClick={e => submitForm()}>Search</Button>
+                <Button onClick={e => clearForm()} color='secondary'>
+                  Clear
+                </Button>
+                {isLoggedIn() && (
+                  <Button onClick={() => setShowDrafts(!showDrafts)}>Show {showDrafts ? 'posts' : 'drafts'}</Button>
                 )}
-              </p>
-            )}
-            {isLoggedIn() && (
-              <Button style={{ marginTop: calcSpacing }} onClick={() => handleDraftEditClick(data.posts[index])}>
-                {showDrafts ? 'Edit' : 'Make Draft'}
-              </Button>
-            )}
+              </div>
+            </StyledForm>
           </Content>
-        ))}
-      </Container>
+          {data?.posts.map(({ title, tags, created, id, isPublished }, index) => (
+            <Content key={index}>
+              <StyledLink hoverColor={darkSubtitleText} to={`/posts/${id}`}>
+                <h1 style={{ marginBottom: calcSpacing }}>{title}</h1>
+              </StyledLink>
+              <p style={{ marginBottom: calcSpacing }}>{moment(created).format('MMMM Do, YYYY')}</p>
+              {tags && (
+                <p style={{ color: darkSubtitleText }}>
+                  {tags.reduce(
+                    (tagString, tag, index) => (tagString += `#${tag}${index !== tags.length - 1 ? ', ' : ''}`),
+                    ''
+                  )}
+                </p>
+              )}
+              {isLoggedIn() && (
+                <Button style={{ marginTop: calcSpacing }} onClick={() => handleDraftEditClick(data.posts[index])}>
+                  {!isPublished ? 'Edit' : 'Make Draft'}
+                </Button>
+              )}
+              {isLoggedIn() && !isPublished && (
+                <Button
+                  style={{ marginLeft: calcSpacing }}
+                  onClick={() => setDeletePost({ id, title })}
+                  color='secondary'
+                >
+                  Delete
+                </Button>
+              )}
+            </Content>
+          ))}
+        </Container>
+        <Modal isOpen={deletePost?.id !== undefined} close={() => setDeletePost(undefined)}>
+          <DeleteContainer spacing={mainSpacing}>
+            <p>
+              Are you sure you want to delete <strong>{deletePost?.title}</strong>?
+            </p>
+            <Button color='secondary' onClick={() => setDeletePost(undefined)}>
+              No
+            </Button>
+            <Button
+              onClick={() => {
+                deleteDraft(undefined, getToken() || undefined)
+                setDeletePost(undefined)
+              }}
+              style={{ marginLeft: calcSpacing, marginTop: calcSpacing }}
+            >
+              Yes
+            </Button>
+          </DeleteContainer>
+        </Modal>
+      </>
     )
   }
 
