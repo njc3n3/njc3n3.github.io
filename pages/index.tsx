387ed2507fe5a27da20275b8311789e857c90093
Nick Coffey <nicholasjcoffey@gmail.com>
2021-04-24 12:26:33 -0500

    tailwind dark mode & switcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react'
+import { useLocalStorage } from '../hooks'
+
 export default function Home() {
-  return <p className='text-9xl'>Hello World</p>
+  const [isDark, setIsDark] = useLocalStorage('isDark', true)
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('light')
+    } else {
+      document.documentElement.classList.add('light')
+      document.documentElement.classList.remove('dark')
+    }
+  }, [isDark])
+
+  return (
+    <div className='flex flex-col items-center'>
+      <p className='text-9xl'>Hello World</p>
+      <button onClick={() => setIsDark(!isDark)} className='px-2 py-1 text-white bg-gray-400 rounded-md dark:bg-black'>
+        {isDark ? 'Light' : 'Dark'}
+      </button>
+    </div>
+  )
 }
